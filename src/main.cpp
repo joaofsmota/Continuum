52f304c3fb245414e7676578ec0a5495eab0e8aa
motaf <112226912+omzandra@users.noreply.github.com>
2023-06-15 11:27:02 +0100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// game.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello game." << endl;
+	return 0;
+}
