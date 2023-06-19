97ad39a8af04470fbdc072224cd67c6ff1e25b24
motaf <112226912+omzandra@users.noreply.github.com>
2023-06-19 17:38:54 +0100

    test opengl

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # $\Large \mathrm{Continuum}$  $\Large \mathrm{engine}$
 # $\large \mathrm{Overview}$
-The Continuum engine is dedicated to integrating and implementing the algorithms outlined in the research papers ([accessible here](https://cescg.org/wp-content/uploads/2018/04/Michelic-Real-Time-Rendering-of-Procedurally-Generated-Planets-2.pdf)) for real-time rendering and procedural generation of planetary surfaces, oceans, and atmospheric conditions. It places a strong emphasis on game-oriented functionalities and features that enable seamless transitions between complex environments, from buildings to cities to atmospheric phenomena and beyond, in order to create a truly immersive experience for players.
+The Continuum engine is dedicated to implementing the algorithms outlined in the research papers ([accessible here](https://cescg.org/wp-content/uploads/2018/04/Michelic-Real-Time-Rendering-of-Procedurally-Generated-Planets-2.pdf)) for real-time rendering and procedural generation of planetary surfaces, oceans, and atmospheric conditions. It places a strong emphasis on game-oriented functionalities and features that enable seamless transitions between complex environments, from buildings to cities to atmospheric phenomena and beyond, in order to create a truly immersive experience for players.
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,150 @@
 //
 
 #include <iostream>
-#include <assert.h>
-#include <memory>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-using namespace std;
+#include "linmath.h"
 
-#define glfw_window_ptr GLFWwindow* 
+#include <stdlib.h>
+#include <stdio.h>
 
-static glfw_window_ptr window = NULL; 
-constexpr int WIN_WIDTH = 1200;
-constexpr int WIN_HEIGHT = 800;
-
-
-int main(int argc, char** argv)
+static const struct
 {
-	if (!glfwInit())
-	{
-		std::cout << "Failed to initialize glfw\n";
-		return -1;
-	}
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
 
-	glfw_window_ptr window = NULL;
+static const char* vertex_shader_text = R"GLSL(
+		#version 110
+		uniform mat4 MVP;
+        attribute vec3 vCol;
+        attribute vec2 vPos;
+        varying vec3 color;
+		void main()
+		{
+			gl_Position = MVP * vec4(vPos, 0.0, 1.0);
+            color = vCol;
+		}
+	)GLSL";
 
-	if (!(window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "game", nullptr, nullptr)))
-	{
-		glfwTerminate();
-		return -1;
-	}
+static const char* fragment_shader_text = R"GLSL(
+		#version 110
+		varying vec3 color;
+		void main()
+		{
+			gl_FragColor = vec4(color, 1.0);
+		}
+	)GLSL";
 
-	bool quit = true;
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
 
-	while (!quit)
-	{
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
-	}
+int main(void)
+{
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwSetErrorCallback(error_callback);
 
-	return 0;
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(1200, 800, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        mat4x4 m, p, mvp;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
